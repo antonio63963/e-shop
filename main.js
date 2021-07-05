dd79c71b5f00bf90f5bdab660be5391f02c2f98a
antonio63963 <4access@ukr.net>
2021-07-05 09:03:28 +0300

    routing

--- main.js
+++ main.js
@@ -2,10 +2,16 @@ require('dotenv').config();
 const  express = require('express');
 const PORT = process.env.PORT || 5000;
 const sequelize = require('./db');
+const cors = require('cors')
 const models = require('./models/models');
+const router = require('./routes')
 
 
 const server = express();
+server.use(cors());
+server.use(express.json());
+server.use('/api', router)
+
 const start = async () => {
   try {
     await sequelize.authenticate();
